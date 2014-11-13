d9c0ad4a508cb03b6013c90387a170bf64133249
MarkusH <markushorstmann@hotmail.com>
2014-11-12 20:55:57 -0800

    Fix misc. bugs in quickstart and project files.

--- Devices/RaspberryPiGateway/Scripts/SCPRPI.cmd
+++ Devices/RaspberryPiGateway/Scripts/SCPRPI.cmd
@@ -3,7 +3,7 @@ set prjdir=..\
 set rpi_ip=192.168.1.107
 set rpi_usr=pi
 set rpi_pw=raspberry
-%puttydir%pscp -pw %rpi_pw% %prjdir%bin\Debug\CloudPI.exe %rpi_usr%@%rpi_ip%:RaspberryPiGateway/
+%puttydir%pscp -pw %rpi_pw% %prjdir%bin\Debug\RaspberryPiGateway.exe %rpi_usr%@%rpi_ip%:RaspberryPiGateway/
 %puttydir%pscp -pw %rpi_pw% %prjdir%bin\Debug\Amqp.Net.dll %rpi_usr%@%rpi_ip%:RaspberryPiGateway/
 %puttydir%pscp -pw %rpi_pw% %prjdir%bin\Debug\Newtonsoft.Json.dll %rpi_usr%@%rpi_ip%:RaspberryPiGateway/
 %puttydir%pscp -pw %rpi_pw% %prjdir%Scripts\autorun.sh %rpi_usr%@%rpi_ip%:RaspberryPiGateway/autorun.sh
