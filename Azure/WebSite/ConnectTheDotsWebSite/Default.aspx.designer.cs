5863f172b50d98225d54d6bbb41a53d3571b6a98
olivierbloch <olivier.bloch@hotmail.com>
2015-02-11 17:39:04 +0100

    Revert "merging changes with main project"
    This reverts commit 52f2070fb32d8b8fad3e2ffdfd223d2a75e1b94a.

--- Azure/WebSite/ConnectTheDotsWebSite/Default.aspx.designer.cs
+++ Azure/WebSite/ConnectTheDotsWebSite/Default.aspx.designer.cs
@@ -11,5 +11,14 @@ namespace ConnectTheDotsWebSite {
     
     
     public partial class Default {
+        
+        /// <summary>
+        /// form2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form2;
     }
 }
