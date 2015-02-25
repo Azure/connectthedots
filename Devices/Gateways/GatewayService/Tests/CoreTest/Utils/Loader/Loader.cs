a15f036d28f4e01459747e0195f6a2a3b1cce8df
lt72 <lorenzte@microsoft.com>
2015-02-26 00:09:44 +0100

    added AMQP config to core test

--- Devices/Gateways/GatewayService/Tests/CoreTest/Utils/Loader/Loader.cs
+++ Devices/Gateways/GatewayService/Tests/CoreTest/Utils/Loader/Loader.cs
@@ -4,6 +4,15 @@ using System.Configuration;
 
 namespace CoreTest.Utils.Loader
 {
+    internal class AMQPConfig
+    {
+        public string AMQPSAddress;
+        public string EventHubName;
+        public string EventHubMessageSubject;
+        public string EventHubDeviceId;
+        public string EventHubDeviceDisplayName;
+    };
+
     internal static class Loader
     {
         internal static IList<String> GetSources() 
@@ -21,6 +30,94 @@ namespace CoreTest.Utils.Loader
             }
 
             return dataIntakes;
+        }
+
+        internal static AMQPConfig GetAMQPConfig( )
+        {
+            AMQPServiceConfigSection section = ConfigurationManager.GetSection( "AMQPServiceConfig" ) as AMQPServiceConfigSection;
+            AMQPConfig configData = null;
+
+            if ( section != null )
+            {
+                configData = new AMQPConfig
+                {
+                    AMQPSAddress = section.AMQPSAddress,
+                    EventHubName = section.EventHubName,
+                    EventHubMessageSubject = section.EventHubMessageSubject,
+                    EventHubDeviceId = section.EventHubDeviceId,
+                    EventHubDeviceDisplayName = section.EventHubDeviceDisplayName
+                };
+            }
+
+            return configData;
+        }
+    }
+
+    internal class AMQPServiceConfigSection : ConfigurationSection
+    {
+        [ConfigurationProperty( "AMQPSAddress", DefaultValue = "AMQPSAddress", IsRequired = true )]
+        public string AMQPSAddress
+        {
+            get
+            {
+                return (string)this[ "AMQPSAddress" ];
+            }
+            set
+            {
+                this[ "AMQPSAddress" ] = value;
+            }
+        }
+
+        [ConfigurationProperty( "EventHubName", DefaultValue = "EventHubName", IsRequired = true )]
+        public string EventHubName
+        {
+            get
+            {
+                return (string)this[ "EventHubName" ];
+            }
+            set
+            {
+                this[ "EventHubName" ] = value;
+            }
+        }
+
+        [ConfigurationProperty( "EventHubMessageSubject", DefaultValue = "EventHubMessageSubject", IsRequired = true )]
+        public string EventHubMessageSubject
+        {
+            get
+            {
+                return (string)this[ "EventHubMessageSubject" ];
+            }
+            set
+            {
+                this[ "EventHubMessageSubject" ] = value;
+            }
+        }
+
+        [ConfigurationProperty( "EventHubDeviceId", DefaultValue = "EventHubDeviceId", IsRequired = true )]
+        public string EventHubDeviceId
+        {
+            get
+            {
+                return (string)this[ "EventHubDeviceId" ];
+            }
+            set
+            {
+                this[ "EventHubDeviceId" ] = value;
+            }
+        }
+
+        [ConfigurationProperty( "EventHubDeviceDisplayName", DefaultValue = "EventHubDeviceDisplayName", IsRequired = true )]
+        public string EventHubDeviceDisplayName
+        {
+            get
+            {
+                return (string)this[ "EventHubDeviceDisplayName" ];
+            }
+            set
+            {
+                this[ "EventHubDeviceDisplayName" ] = value;
+            }
         }
     }
 
