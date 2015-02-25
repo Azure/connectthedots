b3345d095c11fea58b1d43e861147ca47638447d
Dinar Gainitdinov <v-digain@microsoft.com>
2015-02-25 17:17:16 +0300

    Updated deploying script.

--- Devices/Gateways/GatewayService/Scripts/SCPRPI.cmd
+++ Devices/Gateways/GatewayService/Scripts/SCPRPI.cmd
@@ -4,21 +4,17 @@ set prjdir=..\
 set rpi_ip=<your Raspberry PI address here>
 set rpi_usr=<your Raspberry PI user name>
 set rpi_pw=<your Raspberry PI password>
+set Configuration=Debug
 
-rem echo Creating GatewayService directory
-rem echo mkdir GatewayService > %temp%\gatewayservicemkdir.tmp
-rem %puttydir%putty %rpi_usr%@%rpi_ip% -pw %rpi_pw% -m %temp%\gatewayservicemkdir.tmp
+echo Creating GatewayService directory
+echo mkdir GatewayService > %temp%\gatewayservicemkdir.tmp
+%puttydir%putty %rpi_usr%@%rpi_ip% -pw %rpi_pw% -m %temp%\gatewayservicemkdir.tmp
 
 echo Copying Gateway files
-%puttydir%pscp -pw %rpi_pw% %prjdir%WindowsService\bin\Release\Amqp.Net.dll %rpi_usr%@%rpi_ip%:GatewayService/
-%puttydir%pscp -pw %rpi_pw% %prjdir%WindowsService\bin\Release\Gateway.dll %rpi_usr%@%rpi_ip%:GatewayService/
-%puttydir%pscp -pw %rpi_pw% %prjdir%WindowsService\bin\Release\GatewayService.exe %rpi_usr%@%rpi_ip%:GatewayService/
-%puttydir%pscp -pw %rpi_pw% %prjdir%WindowsService\bin\Release\GatewayService.exe.config %rpi_usr%@%rpi_ip%:GatewayService/
-%puttydir%pscp -pw %rpi_pw% %prjdir%WindowsService\bin\Release\Newtonsoft.Json.dll %rpi_usr%@%rpi_ip%:GatewayService/
-%puttydir%pscp -pw %rpi_pw% %prjdir%WindowsService\bin\Release\SharedInterfaces.dll %rpi_usr%@%rpi_ip%:GatewayService/
+%puttydir%pscp -pw %rpi_pw% %prjdir%WindowsService\bin\%Configuration%\*.* %rpi_usr%@%rpi_ip%:GatewayService/
 
-%puttydir%pscp -pw %rpi_pw% %prjdir%DataIntakes\SerialPortListener\bin\Release\SerialPortListener.dll %rpi_usr%@%rpi_ip%:GatewayService/
-%puttydir%pscp -pw %rpi_pw% %prjdir%DataIntakes\SocketListener\bin\Release\SocketListener.dll %rpi_usr%@%rpi_ip%:GatewayService/
+%puttydir%pscp -pw %rpi_pw% %prjdir%DataIntakes\SerialPortListener\bin\%Configuration%\SerialPortListener.dll %rpi_usr%@%rpi_ip%:GatewayService/
+%puttydir%pscp -pw %rpi_pw% %prjdir%DataIntakes\SocketListener\bin\%Configuration%\SocketListener.dll %rpi_usr%@%rpi_ip%:GatewayService/
 
 echo copying scripts
 rem %puttydir%pscp -pw %rpi_pw% %prjdir%Scripts\autorun.sh %rpi_usr%@%rpi_ip%:GatewayService/
