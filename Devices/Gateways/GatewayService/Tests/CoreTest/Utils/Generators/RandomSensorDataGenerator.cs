43a2f2d0b11bce8591e7c379f1c6aeee5e0936e4
Dinar Gainitdinov <v-digain@microsoft.com>
2015-02-26 12:02:48 +0300

    Updated data generation for testing.

--- Devices/Gateways/GatewayService/Tests/CoreTest/Utils/Generators/RandomSensorDataGenerator.cs
+++ Devices/Gateways/GatewayService/Tests/CoreTest/Utils/Generators/RandomSensorDataGenerator.cs
@@ -6,17 +6,38 @@ namespace CoreTest.Utils.Generators
     public static class RandomSensorDataGenerator
     {
         //Simple generator for initial testing
+        private const int DEVICE_COUNT = 4;
+
+        private static readonly Random _Random = new Random();
+        private static readonly string[] _Guids = new string[DEVICE_COUNT];
+        private static readonly string[] _MeasureName = new string[DEVICE_COUNT];
+        private static readonly string[] _UnitOfMeasure = new string[DEVICE_COUNT];
+        private static readonly string[] _DisplayName = new string[DEVICE_COUNT];
+
+        static RandomSensorDataGenerator()
+        {
+            for (int i = 0; i < DEVICE_COUNT; ++i)
+            {
+                int rint = i % 2;
+
+                _Guids[i] = (new Guid(_Random.Next(), 0, 0, new byte[8])).ToString();
+                _MeasureName[i] = rint == 0 ? "length" : "time";
+                _UnitOfMeasure[i] = rint == 0 ? "m" : "s";
+                _DisplayName[i] = "Sensor" + i + (rint == 0 ? "m" : "s");
+            }
+        }
+
         public static SensorDataContract Generate()
         {
-            Random r = new Random();
-            int rint = r.Next() % 2, cint = r.Next() % 2;
+            int device = _Random.Next() % DEVICE_COUNT;
+
             SensorDataContract sensorData = new SensorDataContract
             {
-                MeasureName = rint == 0 ? "length" : "time",
-                UnitOfMeasure = rint == 0 ? "m" : "s",
-                DisplayName = "Sensor" + cint + (rint == 0 ? "m" : "s"),
-                Guid = new Guid().ToString(),
-                Value = r.Next() % 1000 - 500,
+                MeasureName = _MeasureName[device],
+                UnitOfMeasure = _UnitOfMeasure[device],
+                DisplayName = _DisplayName[device],
+                Guid = _Guids[device],
+                Value = _Random.Next() % 1000 - 500,
                 Location = "here",
                 Organization = "contoso",
                 TimeCreated = DateTime.UtcNow
