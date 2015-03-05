782135f2c503e4265027b670fb5cf1c416c7fa0d
spyrossak <spyros@spyros.com>
2015-03-05 13:28:03 -0800

    fixed config and deploy script

--- Devices/Gateways/GatewayService/Scripts/RaspberryPi/deploy.cmd
+++ Devices/Gateways/GatewayService/Scripts/RaspberryPi/deploy.cmd
@@ -1,10 +1,10 @@
 rem @echo off
 set puttydir="C:\software\PuTTY\"
-set prjdir=..\
+set prjdir=..\..\
 set rpi_ip=10.121.204.248
 set rpi_usr=pi
 set rpi_pw=raspberry
-set Configuration=Release
+set Configuration=Debug
 set GW_Home=GatewayService
 set Staging=%GW_Home%/Staging
 set PUTTY_CMD=%puttydir%putty %rpi_usr%@%rpi_ip% -pw %rpi_pw% 
@@ -25,8 +25,8 @@ echo Copying Gateway files
 %PSCP_CMD% %prjdir%DeviceAdapters\Socket\bin\%Configuration%\Microsoft.ConnectTheDots.SocketAdapter.dll         %rpi_usr%@%rpi_ip%:%Staging%/
 
 echo copying scripts
-%PSCP_CMD% %prjdir%Scripts\autorun.sh %rpi_usr%@%rpi_ip%:%Staging%/
-%PSCP_CMD% %prjdir%Scripts\runonce.sh %rpi_usr%@%rpi_ip%:%Staging%/
+%PSCP_CMD% %prjdir%Scripts\RaspberryPi\autorun.sh %rpi_usr%@%rpi_ip%:%Staging%/
+%PSCP_CMD% %prjdir%Scripts\RaspberryPi\runonce.sh %rpi_usr%@%rpi_ip%:%Staging%/
 
 echo Marking autorun.sh as executable
 del /f %temp%\rpigatewayautorunx.tmp
