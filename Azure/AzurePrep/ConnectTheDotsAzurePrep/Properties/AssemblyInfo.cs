dffc8b0ac3dc194ad5009ce869f4601fe30e569a
Dinar Gainitdinov <v-digain@microsoft.com>
2015-03-12 17:27:30 +0300

    Updated CloudDeploy.

--- Azure/AzurePrep/ConnectTheDotsAzurePrep/Properties/AssemblyInfo.cs
+++ Azure/AzurePrep/ConnectTheDotsAzurePrep/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConnectTheDotsAzurePrep")]
+[assembly: AssemblyTitle("AzurePrep")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConnectTheDotsAzurePrep")]
+[assembly: AssemblyProduct("AzurePrep")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
