11651eb9b495a38ad098b34695426613941fccc5
Dinar Gainitdinov <v-digain@microsoft.com>
2015-03-17 08:25:52 +0300

    Set EventLogger to be used from MS runtime only, NLog - from mono.

--- Devices/Gateways/GatewayService/WindowsService/Utils/Logger/EventLogger.cs
+++ Devices/Gateways/GatewayService/WindowsService/Utils/Logger/EventLogger.cs
@@ -32,42 +32,40 @@ namespace Microsoft.ConnectTheDots.GatewayService
 
     //--//
 
-    public class EventLogger : ILogger
+    public class WindowsEventLogger : ILogger
     {
         #region Singleton implementation
 
-        private static readonly object  _syncRoot = new object();
+        private static readonly object  _syncRoot = new object( );
 
         //--//
 
-        private static EventLogger      _eventLogger;
+        private static ILogger          _WindowsEventLoggerInstance;
         private static EventLog         _eventLog;
-        private static NLog.Logger      _NLog;
 
         //--//
-
-        internal static EventLogger Instance
+        
+        public static ILogger Instance
         {
             get
             {
-                if( _eventLogger == null )
+                if( _WindowsEventLoggerInstance == null )
                 {
                     lock( _syncRoot )
                     {
-                        if( _eventLogger == null )
+                        if( _WindowsEventLoggerInstance == null )
                         {
-                            _eventLogger = new EventLogger( );
+                            _WindowsEventLoggerInstance = new WindowsEventLogger( );
                         }
                     }
                 }
 
-                return _eventLogger;
+                return _WindowsEventLoggerInstance;
             }
         }
 
-        private EventLogger( )
+        private WindowsEventLogger( )
         {
-            _NLog = LogManager.GetCurrentClassLogger( );
             _eventLog = new EventLog
             {
                 Source = Constants.WindowsServiceName,
@@ -86,19 +84,101 @@ namespace Microsoft.ConnectTheDots.GatewayService
 
         public void Flush( )
         {
+        }
+
+        public void LogError( string logMessage )
+        {
+            _eventLog.WriteEntry( logMessage, EventLogEntryType.Error );
+        }
+
+        public void LogInfo( string logMessage )
+        {
+            _eventLog.WriteEntry( logMessage, EventLogEntryType.Information );
+        }
+    }
+
+    public class NLogEventLogger : ILogger
+    {
+        #region Singleton implementation
+
+        private static readonly object  _syncRoot = new object( );
+
+        //--//
+
+        private static ILogger          _NLogEventLoggerInstance;
+        private static NLog.Logger      _NLog;
+
+        //--//
+
+        public static ILogger Instance
+        {
+            get
+            {
+                if( _NLogEventLoggerInstance == null )
+                {
+                    lock( _syncRoot )
+                    {
+                        if( _NLogEventLoggerInstance == null )
+                        {
+                            _NLogEventLoggerInstance = new NLogEventLogger( );
+                        }
+                    }
+                }
+
+                return _NLogEventLoggerInstance;
+            }
+        }
+
+        private NLogEventLogger( )
+        {
+            _NLog = LogManager.GetCurrentClassLogger( );
+        }
+
+        #endregion
+
+        public void Flush( )
+        {
             LogManager.Flush( );
         }
 
         public void LogError( string logMessage )
         {
             _NLog.Error( logMessage );
-            _eventLog.WriteEntry( logMessage, EventLogEntryType.Error );
         }
 
         public void LogInfo( string logMessage )
         {
             _NLog.Info( logMessage );
-            _eventLog.WriteEntry( logMessage, EventLogEntryType.Information );
+        }
+    }
+
+    public static class EventLogger
+    {
+        private static readonly object  _syncRoot = new object( );
+
+        //--//
+
+        private static ILogger _eventLogger;
+
+        //--//
+
+        public static ILogger Instance
+        {
+            get
+            {
+                if( _eventLogger == null )
+                {
+                    lock( _syncRoot )
+                    {
+                        if( _eventLogger == null )
+                        {
+                            _eventLogger = Platform.IsMono ? NLogEventLogger.Instance : WindowsEventLogger.Instance;
+                        }
+                    }
+                }
+
+                return _eventLogger;
+            }
         }
     }
 }
