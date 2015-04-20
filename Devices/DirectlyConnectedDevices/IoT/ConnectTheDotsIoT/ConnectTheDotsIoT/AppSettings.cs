1eb9b6a435db82157a9b8a3ef8fdb6c79f987c91
Anthony Ngu <anngu@microsoft.com>
2015-04-20 13:00:54 -0700

    Added an example of a direct Windows IoT Application using ConnectTheDots

---
+++ Devices/DirectlyConnectedDevices/IoT/ConnectTheDotsIoT/ConnectTheDotsIoT/AppSettings.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace ConnectTheDotsIoT
+{
+    /// <summary>
+    ///  Class for managing app settings
+    /// </summary>
+    public class AppSettings
+    {
+        // Our settings
+        ApplicationDataContainer localSettings;
+
+        // The key names of our settings
+        const string SettingsSetKeyname = "settingsset";
+        const string ServicebusNamespaceKeyname = "namespace";
+        const string EventHubNameKeyname = "eventhubname";
+        const string KeyNameKeyname = "keyname";
+        const string KeyKeyname = "key";
+        const string DisplayNameKeyname = "displayname";
+        const string OrganizationKeyname = "organization";
+        const string LocationKeyname = "location";
+
+        // The default value of our settings
+        const bool SettingsSetDefault = false;
+        const string ServicebusNamespaceDefault = "";
+        const string EventHubNameDefault = "";
+        const string KeyNameDefault = "";
+        const string KeyDefault = "";
+        const string DisplayNameDefault = "";
+        const string OrganizationDefault = "";
+        const string LocationDefault = "";
+
+        /// <summary>
+        /// Constructor that gets the application settings.
+        /// </summary>
+        public AppSettings()
+        {
+            // Get the settings for this application.
+            localSettings = ApplicationData.Current.LocalSettings;
+        }
+
+        /// <summary>
+        /// Update a setting value for our application. If the setting does not
+        /// exist, then add the setting.
+        /// </summary>
+        /// <param name="Key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool AddOrUpdateValue(string Key, Object value)
+        {
+            bool valueChanged = false;
+
+            // If the key exists
+            if (localSettings.Values.ContainsKey(Key))
+            {
+                // If the value has changed
+                if (localSettings.Values[Key] != value)
+                {
+                    // Store the new value
+                    localSettings.Values[Key] = value;
+                    valueChanged = true;
+                }
+            }
+            // Otherwise create the key.
+            else
+            {
+                localSettings.Values.Add(Key, value);
+                valueChanged = true;
+            }
+            return valueChanged;
+        }
+
+        /// <summary>
+        /// Get the current value of the setting, or if it is not found, set the 
+        /// setting to the default setting.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="Key"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public T GetValueOrDefault<T>(string Key, T defaultValue)
+        {
+            T value;
+
+            // If the key exists, retrieve the value.
+            if (localSettings.Values.ContainsKey(Key))
+            {
+                value = (T)localSettings.Values[Key];
+            }
+            // Otherwise, use the default value.
+            else
+            {
+                value = defaultValue;
+            }
+            return value;
+        }
+
+        /// <summary>
+        /// Save the settings.
+        /// </summary>
+        public void Save()
+        {
+            // keeping the below in case we want to use this code on a Windows Phone 8 device.
+            // With universal Windows Apps, this is no longer necessary as settings are saved automatically
+            //            settings.Save();
+        }
+
+
+        /// <summary>
+        /// Property to get and set a Username Setting Key.
+        /// </summary>
+        public bool SettingsSet
+        {
+            get
+            {
+                return GetValueOrDefault<bool>(SettingsSetKeyname, SettingsSetDefault);
+            }
+            set
+            {
+                if (AddOrUpdateValue(SettingsSetKeyname, value))
+                {
+                    Save();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Property to get and set a Username Setting Key.
+        /// </summary>
+        public string ServicebusNamespace
+        {
+            get
+            {
+                return GetValueOrDefault<string>(ServicebusNamespaceKeyname, ServicebusNamespaceDefault);
+            }
+            set
+            {
+                if (AddOrUpdateValue(ServicebusNamespaceKeyname, value))
+                {
+                    Save();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Property to get and set a Username Setting Key.
+        /// </summary>
+        public string EventHubName
+        {
+            get
+            {
+                return GetValueOrDefault<string>(EventHubNameKeyname, EventHubNameDefault);
+            }
+            set
+            {
+                if (AddOrUpdateValue(EventHubNameKeyname, value))
+                {
+                    Save();
+                }
+            }
+        }
+        /// <summary>
+        /// Property to get and set a Username Setting Key.
+        /// </summary>
+        public string KeyName
+        {
+            get
+            {
+                return GetValueOrDefault<string>(KeyNameKeyname, KeyNameDefault);
+            }
+            set
+            {
+                if (AddOrUpdateValue(KeyNameKeyname, value))
+                {
+                    Save();
+                }
+            }
+        }
+        /// <summary>
+        /// Property to get and set a Username Setting Key.
+        /// </summary>
+        public string Key
+        {
+            get
+            {
+                return GetValueOrDefault<string>(KeyKeyname, KeyDefault);
+            }
+            set
+            {
+                if (AddOrUpdateValue(KeyKeyname, value))
+                {
+                    Save();
+                }
+            }
+        }
+        /// <summary>
+        /// Property to get and set a Username Setting Key.
+        /// </summary>
+        public string DisplayName
+        {
+            get
+            {
+                return GetValueOrDefault<string>(DisplayNameKeyname, DisplayNameDefault);
+            }
+            set
+            {
+                if (AddOrUpdateValue(DisplayNameKeyname, value))
+                {
+                    Save();
+                }
+            }
+        }
+        /// <summary>
+        /// Property to get and set a Username Setting Key.
+        /// </summary>
+        public string Organization
+        {
+            get
+            {
+                return GetValueOrDefault<string>(OrganizationKeyname, OrganizationDefault);
+            }
+            set
+            {
+                if (AddOrUpdateValue(OrganizationKeyname, value))
+                {
+                    Save();
+                }
+            }
+        }
+        /// <summary>
+        /// Property to get and set a Username Setting Key.
+        /// </summary>
+        public string Location
+        {
+            get
+            {
+                return GetValueOrDefault<string>(LocationKeyname, LocationDefault);
+            }
+            set
+            {
+                if (AddOrUpdateValue(LocationKeyname, value))
+                {
+                    Save();
+                }
+            }
+        }
+
+    }
+}
