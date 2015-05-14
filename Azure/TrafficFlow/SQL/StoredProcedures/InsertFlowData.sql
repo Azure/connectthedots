5b691bae337cdb9647243da792dffec7affbc7b8
Dinar Gainitdinov <v-digain@microsoft.com>
2015-05-14 16:38:21 +0300

    Added missing due gitignore config SQL files.

---
+++ Azure/TrafficFlow/SQL/StoredProcedures/InsertFlowData.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[InsertFlowData]
+	@dataList [dbo].[FlowDataTableType] READONLY
+AS
+	insert into [dbo].[FlowData] ([FlowDataId], [Value], [Time])
+		select [FlowDataId], [Value], [Time] from @dataList
+RETURN 0
---
+++ Azure/TrafficFlow/SQL/StoredProcedures/UpdateFlowSources.sql
@@ -0,0 +1,42 @@
+﻿CREATE PROCEDURE [dbo].[UpdateFlowSources]
+	@sourcesList [dbo].[FlowSourcesTableType] READONLY
+AS
+	MERGE INTO [dbo].[FlowSources] AS A 
+    USING ( 
+        SELECT * FROM @sourcesList 
+    ) B ON (A.Id = B.Id) 
+    WHEN MATCHED THEN 
+        UPDATE SET
+			A.[Region] = B.[Region], 
+			A.[StationName] = B.[StationName],
+			A.[LocationDescription] = B.[LocationDescription],
+			A.[LocationDirection] = B.[LocationDirection],
+			A.[LocationLatitude] = B.[LocationLatitude],
+			A.[LocationLongitude] = B.[LocationLongitude],
+			A.[LocationMilePost] = B.[LocationMilePost],
+			A.[LocationRoadName] = B.[LocationRoadName]
+    WHEN NOT MATCHED THEN 
+        INSERT (
+			[Id],
+			[Region],
+			[StationName],
+			[LocationDescription],
+			[LocationDirection],
+			[LocationLatitude],
+			[LocationLongitude],
+			[LocationMilePost],
+			[LocationRoadName]
+		)
+        VALUES
+		(
+			B.[Id],
+			B.[Region],
+			B.[StationName],
+			B.[LocationDescription],
+			B.[LocationDirection],
+			B.[LocationLatitude],
+			B.[LocationLongitude],
+			B.[LocationMilePost],
+			B.[LocationRoadName]
+		);
+RETURN 0
