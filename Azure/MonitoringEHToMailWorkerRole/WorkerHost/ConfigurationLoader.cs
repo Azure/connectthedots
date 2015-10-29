061af8cba0a17597e58fd8ce12e64512870ade32
Dinar Gainitdinov <v-digain@microsoft.com>
2015-10-29 14:40:15 +0300

    Added worker role for monitoring messages in EH with sending mail.

---
+++ Azure/MonitoringEHToMailWorkerRole/WorkerHost/ConfigurationLoader.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+
+
+namespace WorkerHost
+{
+
+    public class MailToConfigSection : ConfigurationSection
+    {
+        [ConfigurationProperty("", IsRequired = true, IsDefaultCollection = true)]
+        public MailToConfigInstanceCollection Instances
+        {
+            get { return (MailToConfigInstanceCollection)this[""]; }
+            set { this[""] = value; }
+        }
+    }
+
+    public class MailToConfigInstanceCollection : ConfigurationElementCollection
+    {
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new MailToConfigInstanceElement();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((MailToConfigInstanceElement)element).Address;
+        }
+    }
+
+    public class MailToConfigInstanceElement : ConfigurationElement
+    {
+        [ConfigurationProperty("address", IsKey = true, IsRequired = true)]
+        public string Address
+        {
+            get
+            {
+                return (string)base["address"];
+            }
+        }
+    }
+
+    public static class ConfigurationLoader
+    {
+        public static IList<string> GetMailToList()
+        {
+            var addresses = new List<string>();
+
+            MailToConfigSection config = ConfigurationManager.GetSection("mailToList") as MailToConfigSection;
+
+            if (config != null)
+            {
+                foreach (MailToConfigInstanceElement e in config.Instances)
+                {
+                    addresses.Add(e.Address);
+                }
+            }
+
+            return addresses;
+        }
+    }
+}
---
+++ Azure/MonitoringEHToMailWorkerRole/WorkerHost/Program.cs
@@ -0,0 +1,102 @@
+﻿//#define DEBUG_LOG
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using SendGrid;
+
+namespace WorkerHost
+{
+    public class WorkerHost : RoleEntryPoint
+    {
+        public class Configuration
+        {
+            public string DeviceEHConnectionString;
+            public string DeviceEHName;
+
+            public string SendGridUserName;
+            public string SendGridPassword;
+
+            public string MessageFromAddress;
+            public string MessageFromName;
+            public string MessageSubject;
+
+            public IList<string> MailToList;
+        }
+
+        private static EventHubReader _eventHubReader;
+        private static Timer _timer;
+        private static Web _SendGridTransportWeb;
+        private static Configuration config;
+
+        static void Main()
+        {
+            StartHost("lchecker");
+        }
+
+        public override void Run()
+        {
+            StartHost("checker");
+        }
+
+        private static void StartHost(string consumerGroupPrefix)
+        {
+            Trace.WriteLine("Starting Worker...");
+#if DEBUG_LOG
+            RoleEnvironment.TraceSource.TraceInformation("Starting Worker...");
+#endif
+            config = new Configuration
+            {
+                DeviceEHConnectionString =
+                    ConfigurationManager.AppSettings.Get("Microsoft.ServiceBus.ConnectionStringDevices"),
+                DeviceEHName = ConfigurationManager.AppSettings.Get("Microsoft.ServiceBus.EventHubDevices"),
+                SendGridUserName = ConfigurationManager.AppSettings.Get("SendGridUserName"),
+                SendGridPassword = ConfigurationManager.AppSettings.Get("SendGridPassword"),
+                MessageFromAddress = ConfigurationManager.AppSettings.Get("MessageFromAddress"),
+                MessageFromName = ConfigurationManager.AppSettings.Get("MessageFromName"),
+                MessageSubject = ConfigurationManager.AppSettings.Get("MessageSubject"),
+                MailToList = ConfigurationLoader.GetMailToList()
+            };
+
+            var credentials = new NetworkCredential(config.SendGridUserName, config.SendGridPassword);
+            _SendGridTransportWeb = new Web(credentials);
+
+            _eventHubReader = new EventHubReader(consumerGroupPrefix, OnMessage);
+
+             Process();
+        }
+
+        public static void Process()
+        {
+            _eventHubReader.Run(config.DeviceEHConnectionString, config.DeviceEHName, string.Empty);
+            _eventHubReader.FailureEvent.WaitOne();
+        }
+
+        private static void OnMessage(string serializedData)
+        {
+            try
+            {
+                if (config.MailToList.Any())
+                {
+                    SendGridMessage myMessage = new SendGridMessage();
+                    myMessage.AddTo(config.MailToList);
+
+                    myMessage.From = new MailAddress(config.MessageFromAddress, config.MessageFromName);
+                    myMessage.Subject = config.MessageSubject;
+                    myMessage.Text = "Mesage Received: \n" + serializedData;
+
+                    _SendGridTransportWeb.DeliverAsync(myMessage).Wait();
+                }
+            }
+            catch (Exception)
+            {
+                Trace.WriteLine("Exception on mail sending...");
+            }
+        }
+    }
+}
