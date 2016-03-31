540dca3f3d8b3c7c4abcbd7d6de1f76ad7b474ce
Olivier Bloch <obloch@microsoft.com>
2016-03-31 09:02:27 -0700

    Updated SensorTag generic sample to use connecthedots npm package

--- Devices/DirectlyConnectedDevices/NodeJS/SensorTag/index.js
+++ Devices/DirectlyConnectedDevices/NodeJS/SensorTag/index.js
@@ -8,8 +8,16 @@ var devicesettings = require('./settings.json');
 // Sensortag utility class
 var worker = require('./sensorWorker.js');
 
-// Init connection to Azure IoT
-connectthedots.init_connection(devicesettings);
-
+var initCallback = function (err) {
+    // Once the connection to Azure IoT Hub is establish you can initialize your hardware and start sending data
+    // This is where you would insert your sensors code
 // Start sensor worker with the send_message callback to the data reception
-worker.start(connectthedots.send_message);
+    worker.start(connectthedots.send_message);
+};
+
+
+// Init connection to Azure IoT
+connectthedots.init_connection(devicesettings, initCallback);
+
+
+
