2b0f914f3121f9889ed12726c1ded7e7105520f9
Olivier Bloch <obloch@microsoft.com>
2016-04-26 23:31:26 -0400

    Added Xamarin.iOS sample

--- Devices/DirectlyConnectedDevices/XamarinSimulatedSensors/XamarinSimulatedSensors/XamarinSimulatedSensors.iOS/ViewController.designer.cs
+++ Devices/DirectlyConnectedDevices/XamarinSimulatedSensors/XamarinSimulatedSensors/XamarinSimulatedSensors.iOS/ViewController.designer.cs
@@ -1,27 +1,101 @@
-﻿//
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
 using System.CodeDom.Compiler;
 using UIKit;
 
 namespace XamarinSimulatedSensors.iOS
 {
-	[Register ("ViewController")]
-	partial class ViewController
-	{
-		[Outlet]
-		[GeneratedCode ("iOS Designer", "1.0")]
-		UIButton Button { get; set; }
-        
-		void ReleaseDesignerOutlets ()
-		{
-			if (Button != null) {
-				Button.Dispose ();
-				Button = null;
-			}
-		}
-	}
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton buttonConnect { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton buttonSend { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblConnectionString { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblHumidity { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblTemperature { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISlider sliderHumidity { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISlider sliderTemperature { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField textConnectionString { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField textDisplayName { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (buttonConnect != null) {
+                buttonConnect.Dispose ();
+                buttonConnect = null;
+            }
+
+            if (buttonSend != null) {
+                buttonSend.Dispose ();
+                buttonSend = null;
+            }
+
+            if (lblConnectionString != null) {
+                lblConnectionString.Dispose ();
+                lblConnectionString = null;
+            }
+
+            if (lblHumidity != null) {
+                lblHumidity.Dispose ();
+                lblHumidity = null;
+            }
+
+            if (lblTemperature != null) {
+                lblTemperature.Dispose ();
+                lblTemperature = null;
+            }
+
+            if (sliderHumidity != null) {
+                sliderHumidity.Dispose ();
+                sliderHumidity = null;
+            }
+
+            if (sliderTemperature != null) {
+                sliderTemperature.Dispose ();
+                sliderTemperature = null;
+            }
+
+            if (textConnectionString != null) {
+                textConnectionString.Dispose ();
+                textConnectionString = null;
+            }
+
+            if (textDisplayName != null) {
+                textDisplayName.Dispose ();
+                textDisplayName = null;
+            }
+        }
+    }
 }
